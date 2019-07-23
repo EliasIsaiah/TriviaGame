6c6b95985feed9e49add0ac034a03876a1b1dd0c
Elias Roussos <eliasIsaiah@outlook.com>
2019-07-23 19:40:55 -0400

    -added divs for timer and current question to index.html -added background picture image file -added more js code for timer, click event, etc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,73 @@
 //trivia
 $(document).ready(function () {
-    let questions;
+
+
+    const game = {
+        correctAnswers: 0,
+        incorrectAnswers: 0,
+        answersDOM: $("div.answers"),
+        incorrectAnswersDOM: $("div.incorrectAnswers"),
+        currentQuestion: "",
+        currentQuestionDOM: $("div.currentQuestion"),
+        questions: null,
+        // counter: null,
+    }
+
+    const timer = {
+        //stolen from stopwatch activity
+        incrementor: null,
+        time: 0,
+        currentTime: null,
+
+        start: function () {
+
+            if (this.incrementor) {
+                this.incrementor = setTimeout(this.count, 1000);
+            }
+        },
+
+        stop: function () {
+            clearTimeout(this.incrementor);
+        },
+
+        count: function () {
+            this.time++;
+            this.currentTime = this.timeConverter(time);
+            $("div.timer").text(this.currentTime);
+
+            this.start();
+        },
+
+        timeConverter: function (t) {
+
+            var minutes = Math.floor(t / 60);
+            var seconds = t - (minutes * 60);
+
+            if (seconds < 10) {
+                seconds = "0" + seconds;
+            }
+
+            if (minutes === 0) {
+                minutes = "00";
+            }
+            else if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return minutes + ":" + seconds;
+        },
+    }
 
     $.getJSON("https://opentdb.com/api.php?amount=10&category=20&type=multiple", function (data) {
-        questions = data;
-        // $.each( data, function( key, val ) {
-        //     questions.push(`key: ${key} val: ${val}`);
-        //     console.log(key + " " + val);
+        game.questions = data.results;
+        // questions = questions.results;
         console.log("success", data)
-        console.log(questions.results[0].question)
-        });
+        console.log(game.questions)
+    });
 
-    // $.ajax({
-    //     type: "GET",
-    //     url: "https://opentdb.com/api.php?amount=10&category=20&type=multiple",
-    //     success: function(data) {
-    //         console.log("success", data);
-    //     }
-    // })
-
+    $("div.timer").on("click", function (event) {
+        timer.incrementor = setTimeout(timer.count, 1000);
+        timer.start();
+        console.log("timer clicked!")
+    })
 });
