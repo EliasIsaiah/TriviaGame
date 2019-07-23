1e5f33b10c36ab0dfc78ae78475d8ab20d6fe49d
Elias Roussos <eliasIsaiah@outlook.com>
2019-07-23 13:32:38 -0400

    -figured out how to access the trivia api -work begins on layouting using boostrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,22 @@
 //trivia
 $(document).ready(function () {
+    let questions;
 
-    $.getJSON("https://opentdb.com/api.php?amount=10&category=20&type=multiple&token=9b8816051b67a694401133ed96f228e4d886fcb413e6b9b25450c10c60857a23", function (data) {
-        var questions = [];
-        $.each( data, function( key, val ) {
-            questions.push(`key: ${key} val: ${val}`);
-            console.log(key + " " + val);
+    $.getJSON("https://opentdb.com/api.php?amount=10&category=20&type=multiple", function (data) {
+        questions = data;
+        // $.each( data, function( key, val ) {
+        //     questions.push(`key: ${key} val: ${val}`);
+        //     console.log(key + " " + val);
+        console.log("success", data)
+        console.log(questions.results[0].question)
         });
-        
-    });
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: "https://opentdb.com/api.php?amount=10&category=20&type=multiple",
+    //     success: function(data) {
+    //         console.log("success", data);
+    //     }
+    // })
 
 });
